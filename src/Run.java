5a84089eb62acd8e897ea87bd202d190228a27cd
Yannasit <624259011@webmail.npru.ac.th>
2021-10-04 14:57:04 +0700

    Initial commit

---
+++ src/Run.java
@@ -0,0 +1,10 @@
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee myEmployee = new Employee();
+		System.out.println(myEmployee.me.home.street);
+	}
+
+}
